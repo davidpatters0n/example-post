ec2c57a3200eb89a2a7d6d3246fe3f30774959dc
Mike Rogers <me@mikerogers.io>
2021-08-09 13:53:22 -0500

    Adding inverse_of to has_many/belongs_to argument

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,4 @@
 class Author < ApplicationRecord
-  has_many :posts, foreign_key: 'special_author_id'
+  has_many :posts, foreign_key: :special_author_id, inverse_of: :author
   accepts_nested_attributes_for :posts
 end
